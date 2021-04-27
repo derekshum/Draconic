cb47c83911aa993ccb05b1a9b5c2df0793e8aff6
derekshum <derekqlshum@gmail.com>
2021-04-27 18:38:47 -0400

    Update sharpenTheBlade.py
    Fixes to use most representative variable and type, slight display updates.

--- sharpenTheBlade.py
+++ sharpenTheBlade.py
@@ -2,19 +2,19 @@
 <drac2>
 if len(&ARGS&) > 0:
     args = &ARGS&
-    cc_request = int(args[0][0]) #ki points to spend
+    cc_request = int(args[0]) #ki points to spend
 else:
     cc_request = 3
 cc_name = "Ki Points"
 cc_value = character().get_cc(cc_name)
 return_string = ""
-if cc_value >= cc_request:
+if cc_request <= cc_value and cc_request >= 1 and cc_request <= 3:
     cc_use = cc_request
-    character().mod_cc(cc_name, -cc_request)
+    character().mod_cc(cc_name, -cc_use)
     return_string = (
         f'-title "{name} uses Sharpen the Blade!" '
         f'-desc "As a bonus action, you can expend up to 3 ki points to grant one kensei weapon you touch a bonus to attack and damage rolls when you attack with it. The bonus equals the number of ki points you spent. This bonus lasts for 1 minute or until you use this feature again. This feature has no effect on a magic weapon that already has a bonus to attack and damage rolls." '
-        f'-f "Weapon Bonus | +{cc_request}|inline" '
+        f'-f "Weapon Bonus | +{cc_use}|inline" '
         )
 else:
     cc_use = 0
@@ -24,7 +24,7 @@ else:
 cc_current = cc_str(cc_name)
 return_string += (
     f'-f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-    f'-footer "{ctx.prefix}{ctx.alias} [# ki (optional, default 3)]"'
+    f'-footer "{ctx.prefix}{ctx.alias} [# ki (optional, 1-3, default 3)]"'
     )
 return return_string
 </drac2>
