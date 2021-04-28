ddac8c8f570e2acd9d8cbab5304f1f1e6e55692f
derekshum <derekqlshum@gmail.com>
2021-04-27 20:22:28 -0400

    Update flurryOfBlows_snippet.py
    Working draconic Flurry of Blows

--- flurryOfBlows_snippet.py
+++ flurryOfBlows_snippet.py
@@ -1,4 +1,15 @@
-!snippet fob {{cc="Ki Points"}}
-{{c=get_cc(cc)}}
-{{' -rr 2' if c else ''}}
-{{mod_cc(cc, -1) if c else ''}} -f "{{f'Flurry of Blows|Immediately after you take the Attack action on your turn, you can spend 1 ki point to make two unarmed strikes as a bonus action.\n\n**{cc} (-1)** {cc_str(cc)}' if c else f'Not enough {cc} for Flurry of Blows.\n\n**{cc}** {cc_str(cc)}'}}"
+!snippet fob tembed
+<drac2>
+cc_name = "Ki Points"
+cc_value = character().get_cc(cc_name)
+if cc_value >= 1:
+    character().mod_cc(cc_name, -1)
+    return_string = (f' -rr {2}'
+        f' -f "Flurry of Blows|Immediately after you take the Attack action on your turn, you can spend 1 ki point to make two unarmed strikes as a bonus action.\n\n**{cc_name} (-1)** {cc_str(cc_name)}"'
+        )
+else:
+    return_string = (f' -f "Not enough {cc_name} for Flurry of Blows.\n\n**{cc_name}** {cc_str(cc_name)}"')
+return return_string
+</drac2>
+    
+    
