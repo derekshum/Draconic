13e5d54d675a28439bb1378eaf40b22db6f689c4
derekshum <derekqlshum@gmail.com>
2021-04-27 21:57:39 -0400

    Update deftStrike.py
    working draconic

--- deftStrike.py
+++ deftStrike.py
@@ -1,30 +1,40 @@
 !alias deft tembed
 <drac2>
+crit = False
 if len(&ARGS&) > 0:
     args = &ARGS&
-    cc_request = int(args[0]) #ki points to spend
-else:
-    cc_request = 3
+    input = args[0].lower()
+    if input == "c" or input == "crit" or input == "critical":
+        crit = True    
+ability_name = "Deft Strike"
 cc_name = "Ki Points"
 cc_value = character().get_cc(cc_name)
 return_string = ""
-if cc_request <= cc_value and cc_request >= 1 and cc_request <= 3:
-    cc_use = cc_request
+if cc_value >= 1:
+    cc_use = 1
     character().mod_cc(cc_name, -cc_use)
+    crit_text = ""
+    die = int(4 + int((MonkLevel + 1) / 6) * 2)
+    die_num = 1
+    if crit:
+        crit_text = " (CRIT!)"
+        die_num = 2
+    damage = vroll(str(die_num) + "d" + str(die))
     return_string = (
-        f'-title "{name} uses Sharpen the Blade!" '
-        f'-desc "As a bonus action, you can expend up to 3 ki points to grant one kensei weapon you touch a bonus to attack and damage rolls when you attack with it. The bonus equals the number of ki points you spent. This bonus lasts for 1 minute or until you use this feature again. This feature has no effect on a magic weapon that already has a bonus to attack and damage rolls." '
-        f'-f "Weapon Bonus | +{cc_use}|inline" '
+        f'-title "{name} uses {ability_name}!" '
+        f'-desc "When you hit a target with a kensei weapon, you can spend 1 ki point to cause the weapon to deal extra damage to the target equal to your Martial Arts die. You can use this feature only once on each of your turns." '
+        f' -f "Damage{crit_text}|{str(damage)}" '
         )
 else:
     cc_use = 0
     return_string = (
-        f'-title "{name} fails to use Sharpen the Blade!" '
+        f' -title "{name} fails to use {ability_name}!" '
+        f' -desc "Not enough Ki." '
         )
 cc_current = cc_str(cc_name)
 return_string += (
     f'-f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-    f'-footer "{ctx.prefix}{ctx.alias} [# ki (optional, 1-3, default 3)]"'
+    f'-footer "{ctx.prefix}{ctx.alias} [crit (optional)]"'
     )
 return return_string
 </drac2>
