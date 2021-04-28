f581732915943efd60c80d548e1b080012e9a4cd
derekshum <derekqlshum@gmail.com>
2021-04-28 17:41:53 -0400

    Update diamondSoul.py
    Fixing substring creation, fixing output to be stable. No save rolling yet. Updating some error orders.

--- diamondSoul.py
+++ diamondSoul.py
@@ -15,21 +15,21 @@ else:
     args = &ARGS&
     input = args[0].lower()
     length = len(input)
-    if input == "strength"[0,length]:
+    if input == "strength"[0:length]:
         save = "Strength"
-    elif input == "dexterity"[0,length]:
+    elif input == "dexterity"[0:length]:
         save = "Dexterity"
-    elif input == "constitution"[0,length]:
+    elif input == "constitution"[0:length]:
         save = "Constitution"
-    elif input == "intelligence"[0,length]:
+    elif input == "intelligence"[0:length]:
         save = "Intelligence"
-    elif input == "wisdom"[0,length]:
+    elif input == "wisdom"[0:length]:
         save = "Wisdom"
-    elif input == "charisma"[0,length]:
+    elif input == "charisma"[0:length]:
         save = "Charisma"
-    elif input == "death"[0,length] or input == "ds":
+    elif input == "death"[0:length] or input == "ds":
         save = "Death"
-    else 
+    else:
         error = True   #indicates invalid save specified
     
     if error:
@@ -41,35 +41,32 @@ else:
     else:
         bonus = 0
         if len(args) > 1:
-            if isinstance(args[1], int):
-                bonus = int(args[1])
-            else:  
-                error = True
-        if error:
-            return_string = (
-                f' -title "{name} fails to use {ability_name}!" '
-                f' -desc "Input {str(input)} is not a bonus. Save bonus must be an integer." '
-                )
-        else:
-            if cc_value >= 1:
-                cc_use = 1
-                character().mod_cc(cc_name, -cc_use)
-                #TODO: save rolling
-                return_string = (
-                    f'-title "{name} uses {ability_name}!" '
-                    f'-desc "Your mastery of ki grants you proficiency in all saving throws.\n\nAdditionally, whenever you make a saving throw and fail, you can spend 1 ki point to reroll it and take the second result." '
-                    f'-f "New {save} Save | " ' #TODO: new save roll displaying
-                    )
-            else:
-                cc_use = 0
+            if not isinstance(args[1], int):
                 return_string = (
                     f' -title "{name} fails to use {ability_name}!" '
-                    f' -desc "Not enough {cc_name}." '
+                    f' -desc "Input {str(input)} is not a bonus. Save bonus must be an integer." '
                     )
+            else:  
+                bonus = int(args[1])
+        if cc_value < 1:
+            cc_use = 0
+            return_string = (
+                f' -title "{name} fails to use {ability_name}!" '
+                f' -desc "Not enough {cc_name}." '
+                )
+        else: 
+            cc_use = 1
+            character().mod_cc(cc_name, -cc_use)
+            #TODO: save rolling
+            return_string = (
+                f' -title "{name} uses {ability_name}!" '
+                f' -desc "Your mastery of ki grants you proficiency in all saving throws.\n\nAdditionally, whenever you make a saving throw and fail, you can spend 1 ki point to reroll it and take the second result." '
+                #f'-f "New {save} Save | " ' #TODO: new save roll displaying
+                )
 cc_current = cc_str(cc_name)
 return_string += (
-        f'-f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-        f'-footer "{ctx.prefix}{ctx.alias} [save] [bonus (optional)]"'
+        f' -f "{cc_name} (-{cc_use})| {cc_current}|inline" '
+        f' -footer "{ctx.prefix}{ctx.alias} [save] [bonus (optional)]" '
         )
 return return_string
 </drac2>
