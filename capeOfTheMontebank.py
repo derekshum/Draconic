d826c54347c1b3e42a37897930233011f8953a12
derekshum <derekqlshum@gmail.com>
2021-05-01 22:31:43 -0400

    Update capeOfTheMontebank.py
    Adding the spell description as the text.

--- capeOfTheMontebank.py
+++ capeOfTheMontebank.py
@@ -17,7 +17,7 @@ else:
     character().mod_cc(cc_name, -cc_use)
     return_string = (
         f' -title "{name} uses their {cc_name} to cast Dimension Door!" '
-        #f' -desc "" '
+        f' -desc "Range: 500ft\nComponents: V\n\nYou teleport yourself from your current location to any other spot within range. You arrive at exactly the spot desired. It can be a place you can see, one you can visualize, or one you can describe by stating distance and direction, such as \"200 feet straight downward\" or \"upward to the northwest at a 45- degree angle, 300 feet.\"\n\nYou can bring along objects as long as their weight doesn\'t exceed what you can carry. You can also bring one willing creature of your size or smaller who is carrying gear up to its carrying capacity. The creature must be within 5 feet of you when you cast this spell.\n\nIf you would arrive in a place already occupied by an object or a creature, you and any creature traveling with you each take 4d6 force damage, and the spell fails to teleport you." '
         )
 cc_current = cc_str(cc_name)
 return_string += (
