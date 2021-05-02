4e83403792ef7e16157df289b7db55a05efc3b0a
derekshum <derekqlshum@gmail.com>
2021-05-01 22:38:09 -0400

    Update capeOfTheMontebank.py
    Changing description to item description instead of spell description.

--- capeOfTheMontebank.py
+++ capeOfTheMontebank.py
@@ -17,7 +17,7 @@ else:
     character().mod_cc(cc_name, -cc_use)
     return_string = (
         f' -title "{name} uses their {cc_name} to cast Dimension Door!" '
-        f' -desc "Range: 500ft\nComponents: V\n\nYou teleport yourself from your current location to any other spot within range. You arrive at exactly the spot desired. It can be a place you can see, one you can visualize, or one you can describe by stating distance and direction, such as \"200 feet straight downward\" or \"upward to the northwest at a 45- degree angle, 300 feet.\"\n\nYou can bring along objects as long as their weight doesn\'t exceed what you can carry. You can also bring one willing creature of your size or smaller who is carrying gear up to its carrying capacity. The creature must be within 5 feet of you when you cast this spell.\n\nIf you would arrive in a place already occupied by an object or a creature, you and any creature traveling with you each take 4d6 force damage, and the spell fails to teleport you." '
+        f' -desc "This cape smells faintly of brimstone. While wearing it, you can use it to cast the dimension door spell as an action. This property of the cape can\'t be used again until the next dawn.\n\nWhen you disappear, you leave behind a cloud of smoke, and you appear in a similar cloud of smoke at your destination. The smoke lightly obscures the space you left and the space you appear in, and it dissipates at the end of your next turn. A light or stronger wind disperses the smoke." '
         )
 cc_current = cc_str(cc_name)
 return_string += (
