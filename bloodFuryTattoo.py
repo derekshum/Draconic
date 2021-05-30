d60e4df4e2576ad64d309b8baa83688ac53c060d
derekshum <derekqlshum@gmail.com>
2021-05-30 00:53:15 -0400

    Working
    No crit or multi charge inputs yet.

--- bloodFuryTattoo.py
+++ bloodFuryTattoo.py
@@ -1,10 +1,10 @@
-!alias bf tembed
-<drac2>
 #!cc create "Blood Fury Tattoo" -max 10 -min 0 -type bubble -reset long -title "Blood Fury Tattoo" -desc "The tattoo has 10 charges, and it regains all expended charges daily at dawn. While this tattoo is on your skin, you gain the following benefits: 
 
 #- When you hit a creature with a weapon attack, you can expend a charge to deal an extra 4d6 necrotic damage to the target, and you regain a number of hit points equal to the necrotic damage dealt. 
 #- When a creature you can see damages you, you can expend a charge and use your reaction to make a melee attack against that creature, with advantage on your attack roll."
 
+!alias bf tembed
+<drac2>
 cc_name = "Blood Fury Tattoo"
 cc_value = character().get_cc(cc_name)
 return_string = ""
@@ -20,18 +20,29 @@ else:
     cc_recharge_change = 1
     character().mod_cc(cc_name, -cc_use)
     damage = vroll("4d6")
-    missing_hp = character().max_hp - character().hp
-    if damage.total >= missing_hp: 
-        healing = damage
-        unused_healing = 0
-    else: 
-        healing = missing_hp
-        unused_healing = damage = missing_hp
     return_string = (
         f' -title "{name} makes a Bloodthirsty Strike!" '
         f' -desc "When you hit a creature with a weapon attack, you can expend a charge to deal an extra 4d6 necrotic damage to the target, and you regain a number of hit points equal to the necrotic damage dealt." '
         f' -f "Damage|{str(damage)}|inline" '    #TODO f' -f "Damage{crit_text}|{str(damage)}|inline" '
         )
+    former_hp = character().hp
+    missing_hp = character().max_hp - former_hp
+    if damage.total <= missing_hp: 
+        character().modify_hp(damage.total)
+        return_string += (
+            f' -f "Healing|{str(former_hp)}+{str(damage.total)}={str(character().hp)}/{str(character().max_hp)}|inline" '
+            )
+    elif missing_hp >= 0: 
+        character().modify_hp(missing_hp)
+        return_string += (
+            f' -f "Healing|{str(former_hp)}+{str(missing_hp)}={str(character().hp)}/{str(character().max_hp)}|inline" '
+            f' -f "Unused healing|!hp {str(damage.total - missing_hp)}|inline" '
+            )
+    else: 
+        return_string += (
+            f' -f "Healing|{str(former_hp)}+0={str(character().hp)}/{str(character().max_hp)}|inline" '
+            f' -f "Unused healing|!hp {str(damage.total)}|inline" '
+            )    
 cc_current = cc_str(cc_name)
 return_string += (
     f' -f "{cc_name} (-{cc_use})| {cc_current}|inline" '
