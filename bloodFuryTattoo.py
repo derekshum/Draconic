aeb39286ea5e864c4db0b4a0da740ffe38a36515
derekshum <derekqlshum@gmail.com>
2021-05-29 20:19:53 -0400

    Basic working version
    some healing amount calcs but not applied, displays damage

--- bloodFuryTattoo.py
+++ bloodFuryTattoo.py
@@ -19,14 +19,23 @@ else:
     cc_use = 1
     cc_recharge_change = 1
     character().mod_cc(cc_name, -cc_use)
+    damage = vroll("4d6")
+    missing_hp = character().max_hp - character().hp
+    if damage.total >= missing_hp: 
+        healing = damage
+        unused_healing = 0
+    else: 
+        healing = missing_hp
+        unused_healing = damage = missing_hp
     return_string = (
-        f' -title "{name} uses their {cc_name} to " '   #TODO
-        f' -desc "" '
+        f' -title "{name} makes a Bloodthirsty Strike!" '
+        f' -desc "When you hit a creature with a weapon attack, you can expend a charge to deal an extra 4d6 necrotic damage to the target, and you regain a number of hit points equal to the necrotic damage dealt." '
+        f' -f "Damage|{str(damage)}|inline" '    #TODO f' -f "Damage{crit_text}|{str(damage)}|inline" '
         )
 cc_current = cc_str(cc_name)
 return_string += (
     f' -f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-    f' -footer "{ctx.prefix}{ctx.alias}" '  #TODO
+    f' -footer "{ctx.prefix}{ctx.alias}" '  #TODO add crits
     )
 return return_string
 </drac2>
