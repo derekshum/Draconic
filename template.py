986d7a7234e89125e927c8e6f2d6d30e9714b4d3
derekshum <derekqlshum@gmail.com>
2021-08-18 13:31:52 -0400

    Update template.py
    adding trailing space

--- template.py
+++ template.py
@@ -26,7 +26,7 @@ else:
 cc_current = cc_str(cc_name)
 return_string += (
     f'-f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-    f'-footer "{ctx.prefix}{ctx.alias}"'    #TODO: add any additional inputs
+    f'-footer "{ctx.prefix}{ctx.alias}" '    #TODO: add any additional inputs
     )
 return return_string
 </drac2>
