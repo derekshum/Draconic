df0ba0c8e483c9fd71fb174726904938cf69c163
derekshum <derekqlshum@gmail.com>
2021-09-05 21:47:10 -0400

    Update arrow3r2_snippet.py
    Uses remaining +3 Arrows, denotes unique case where that could result in incorrect damage.

--- arrow3r2_snippet.py
+++ arrow3r2_snippet.py
@@ -3,11 +3,22 @@
 num = 2
 cc_name = "+3 Arrows"
 cc_value = character().get_cc(cc_name)
-if cc_value < num:
-    1 = 0   #not enough +3 Arrows left
-cc_use = num
+n = min(cc_value, num)
+return_string = f'-rr {num}'
+cc_use = n
 character().mod_cc(cc_name, -cc_use)
-return_string = f'-b 3 -d 3 -rr {num}'
+if n == 0:  
+    return_string += f' -f "No +3 Arrows Left|All of these attack were made with mundane arrows." '
+else:
+    return_string += f' -b{n} 3 -d{n} 3'
+    if n < num:
+        p3 = ""
+        if n > 1:
+            p3 = "s"
+        m = ""
+        if num - n > 1:
+            m = "s"
+        return_string += f' -f "Not Enough +3 Arrows Left|These attacks were made with {n} +3 arrow{p3} and {num - n} mundane arrow{m}. The mundane arrow damage will be incorrect if any of the +3 arrows missed." '
 cc_current = cc_str(cc_name)
 return_string += f' -f "{cc_name} (-{cc_use})| {cc_current}|inline"'
 return return_string
