29af545276b1827a793b5e2ac2ee4fd662271f51
derekshum <derekqlshum@gmail.com>
2021-09-10 17:06:40 -0400

    Update radiantWeapon.py
    Adding possibility of bonus from things like All-Purpose Tool.

--- radiantWeapon.py
+++ radiantWeapon.py
@@ -1,6 +1,10 @@
 !alias rad tembed
 <drac2>
 ability_name = "Radiant Weapon"
+bonus = 0
+if len(&ARGS&) > 0:
+    args = &ARGS&
+    bonus = int(args[0]) #bonus
 cc_name = ability_name
 cc_value = character().get_cc(cc_name)
 return_string = ""
@@ -9,7 +13,7 @@ if cc_value >= 1:
     character().mod_cc(cc_name, -cc_use)
     return_string = (
         f'-title "{name} uses their {ability_name}!" '
-        f'-desc "As a reaction immediately after being hit by an attack, the wielder can expend 1 charge and cause the attacker to be blinded until the end of the attacker\'s next turn, unless the attacker succeeds on a **DC {str(get_raw().spellbook.dc)}** Constitution saving throw." '
+        f'-desc "As a reaction immediately after being hit by an attack, the wielder can expend 1 charge and cause the attacker to be blinded until the end of the attacker\'s next turn, unless the attacker succeeds on a **DC {int(get_raw().spellbook.dc) + bonus}** Constitution saving throw." '
         )
 else:
     cc_use = 0
