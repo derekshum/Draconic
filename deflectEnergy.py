a0de1ef3db6a1ab96a864afc5efb5cc25196d84d
derekshum <derekqlshum@gmail.com>
2021-09-12 22:22:26 -0400

    Update deflectEnergy.py
    fixing die size

--- deflectEnergy.py
+++ deflectEnergy.py
@@ -3,8 +3,7 @@
 ability_name = "Deflect Energy"
 return_string = ""
 if MonkLevel >= 11:
-    die = int(4 + int((MonkLevel + 1) / 6) * 2)
-    damage = vroll("d" + str(die) + "+" + str(wisdomMod))
+    damage = vroll("d10+" + str(wisdomMod))
     return_string = (
         f' -title "{name} uses {ability_name}!"'
         f' -desc "When you take acid, cold, fire, force, lightning, or thunder damage, you can use your reaction to deflect it. When you do so, the damage you take is reduced by 1d10 + your Wisdom modifier (minimum reduction of 1)."'
