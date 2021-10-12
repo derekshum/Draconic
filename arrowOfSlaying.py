048bc02caa4c236c032fdc3eeeb37348b9bd11f0
derekshum <derekqlshum@gmail.com>
2021-10-12 12:12:32 -0400

    Update arrowOfSlaying.py
    Added all primary creature types, removed multi-arrow functionality. Added todos.

--- arrowOfSlaying.py
+++ arrowOfSlaying.py
@@ -4,6 +4,11 @@ TODO for adding new types: create cc and add type to arguments for alias
 
 Once an arrow of slaying deals its extra damage to a creature, it becomes a nonmagical arrow."
 
+#TODO: 
+    - add unordered input handling if implementing below
+    - add vuln and resist
+    - add multi-arrow functionality (loop, list, multiline display?)
+
 !alias slay 
 
 tembed
@@ -14,43 +19,57 @@ if len(&ARGS&) > 0:
     l = len(type)
     if type == "aberrations"[0:l]:
         type = "Aberration"
+    elif type == "beasts"[0:l]:
+        type = "Beast"
+    elif type == "celestials"[0:l]:
+        type = "Celestial"
     elif type == "dragons"[0:l]:
         type = "Dragon"
+    elif type == "elementals"[0:l]:
+        type = "Elemental"
+    elif type == "feys"[0:l]:
+        type = "Fey"
+    elif type == "fiends"[0:l]:
+        type = "Fiend"
+    elif type == "giants"[0:l]:
+        type = "Giant"
+    elif type == "humanoids"[0:l]:
+        type = "Humanoid"
+    elif type == "monstrositys"[0:l] or type == "monstrosities"[0:l] :
+        type = "Monstrosity"
+    elif type == "oozes"[0:l]:
+        type = "Ooze"
+    elif type == "plants"[0:l]:
+        type = "Plant"
+    elif type == "undeads"[0:l]:
+        type = "Undead"
     else:
         rs = (
             f' -title "Specified type of Arrow of Slaying is not enabled!" '
             f' -desc "Correct specification or update this alias." ' 
             )
         return rs
-    num = 1
-    if len(&ARGS&) > 1:
-        num = int(args[1])
     cc_name = "Arrows of " + type + " Slaying"
     cc_val = character().get_cc(cc_name)
-    
+    num = 1
+    rs = ""
     if cc_val >= num:
         cc_use = num
         character().mod_cc(cc_name, -cc_use)
-        rs = f'-desc "If a creature belonging to the type, race, or group associated with an arrow of slaying takes damage from the arrow, the creature must make a **DC 17 Constitution saving throw**, taking an extra 6d10 piercing damage on a failed save, or half as much extra damage on a successful one.\n\nOnce an arrow of slaying deals its extra damage to a creature, it becomes a nonmagical arrow." '
-        if num == 1:
-            rs += f'-title "{name} looses an Arrow of {type} Slaying!" '
-            dmg = vroll("6d10[magical piercing]") 
-            rs += f'-f "Damage (DC 17 Con, Save for Half)|{dmg} (Half: `{int(dmg.total/2)}`)" '
-        else:
-            rs += f'-title "{name} looses {num} Arrows of {type} Slaying!" '
-            for i in range(num):
-                dmg = vroll("6d10[magical piercing]")
-                rs += f'-f "Damage {(i + 1)} (DC 17 Con, Save for Half)|{dmg} (Half: `{int(dmg.total/2)}`)" '
+        rs += f'-title "{name} looses an Arrow of {type} Slaying!" '
+        rs += f'-desc "If a creature belonging to the type, race, or group associated with an arrow of slaying takes damage from the arrow, the creature must make a **DC 17 Constitution saving throw**, taking an extra 6d10 piercing damage on a failed save, or half as much extra damage on a successful one.\n\nOnce an arrow of slaying deals its extra damage to a creature, it becomes a nonmagical arrow." '
+        dmg = vroll("6d10[magical piercing]") 
+        rs += f'-f "Damage (DC 17 Con, Save for Half)|{dmg} (Half: `{int(dmg.total/2)}`)" '
     else:
         cc_use = 0
-        rs = (
-            f'-title "{name} fails to loose any Arrows of {type} Slaying!" '
-            f'-desc "Insufficient arrows remaining." '
+        rs += (
+            f'-title "{name} fails to loose an Arrow of {type} Slaying!" '
+            f'-desc "No arrows remaining." '
             )
     cc_current = cc_str(cc_name)
     rs += (
-        f'-f "{cc_name} (-{cc_use})| {cc_current}" '
-        f'-footer "{ctx.prefix}{ctx.alias} [type(required)] [num]" '
+        f'-f "{cc_name} (-{cc_use})|{cc_current}|inline" '
+        f'-footer "{ctx.prefix}{ctx.alias} [type (required)]" '
         )
 else:
     rs = (
