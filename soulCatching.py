6ba85ed8494758a0cfa78813e5140748f420a801
derekshum <derekqlshum@gmail.com>
2021-10-28 10:33:12 -0400

    edits to bf, adding soul catching

--- soulCatching.py
+++ soulCatching.py
@@ -1,10 +1,10 @@
 tembed
 <drac2>
-die_num = 4  #num d6
+die_num = 2  #num d10s
 crit_text = ""
-cc_request = 1
 isResistant = False
 isVulnerable = False
+noHeal = False
 for input in &ARGS&:
     input = input.lower()
     length = len(input)
@@ -12,43 +12,33 @@ for input in &ARGS&:
         die_num = 2 * die_num
         crit_text = " (CRIT!)"
     elif input.isnumeric():
-        cc_request = int(input)
-        die_num = cc_request * die_num
+        die_num = 2 * int(input)
     elif input == "resistant"[0:length] or input == "resistance"[0:length]:
         isResistant = True
     elif input == "vulnerable"[0:length] or input == "vulnerability"[0:length]:
         isVulnerable = True
-cc_name = "Blood Fury Tattoo"
-cc_value = character().get_cc(cc_name)
+    elif input == "nohealing"[0:length] or input == "advantage"[0:length] or input == "gainadvantage"[0:length] or input == "storeadvantage"[0:length]:
+        noHeal = True
 return_string = ""
-if cc_value < cc_request:
-    cc_use = 0
-    return_string = (
-        f' -title "{name} fails to use of their {cc_name}!" '
-        f' -desc "Your {cc_name} does not have {str(cc_request)} remaining charges." '
-        )
-else:
-    cc_use = cc_request
-    character().mod_cc(cc_name, -cc_use)
-    roll_string = str(die_num) + "d6"
-    if isResistant:
-        roll_string += "/2"
-        if cc_use > 1:
-            return_string += f' -f "Calculation Uncertainty|Multiple resisted strikes may have a lower total than displayed due to rounding down odds." '
-    if isVulnerable:
-        roll_string += "*2"
-    roll_string += "[necrotic]"
-    damage = vroll(roll_string)
-    if cc_use == 1:
-        num_text_1 = "a"
-        num_text_2 = ""
-    else:
-        num_text_1 = str(cc_use)
-        num_text_2 = "s"
-    
+roll_string = str(die_num) + "d10"
+if isResistant:
+    roll_string += "/2"
+    if (die_num > 2 and crit_text == "") or die_num > 4 :
+        return_string += f' -f "Calculation Uncertainty|Multiple resisted attacks may have a lower total than displayed due to rounding down odds." '
+if isVulnerable:
+    roll_string += "*2"
+roll_string += "[force]"
+damage = vroll(roll_string)  
+if noHeal:
     return_string += (
-        f' -title "{name} makes {num_text_1} Bloodthirsty Strike{num_text_2}!" '
-        f' -desc "When you hit a creature with a weapon attack, you can expend a charge to deal an extra 4d6 necrotic damage to the target, and you regain a number of hit points equal to the necrotic damage dealt." '
+        f' -title "{name} attacks with their Gloves of Soul Catching, storing advantage!" '
+        f' -desc "After making a successful unarmed strike while wearing these gloves, you can use the gloves to deal an extra 2d10 force damage to the target, and you can choose to gain advantage on one attack roll, ability check, or saving throw you make before the end of your next turn." '
+        f' -f "Damage{crit_text}|{str(damage)}|inline" '
+        )
+else: 
+    return_string += (
+        f' -title "{name} attacks with their Gloves of Soul Catching, regaining health!" '
+        f' -desc "After making a successful unarmed strike while wearing these gloves, you can use the gloves to deal an extra 2d10 force damage to the target, and you regain a number of hit points equal to the force damage dealt. " '
         f' -f "Damage{crit_text}|{str(damage)}|inline" '
         )
     former_hp = character().hp
@@ -69,10 +59,8 @@ else:
             f' -f "Healing|{str(former_hp)} + 0 = {str(character().hp)}/{str(character().max_hp)}|inline" '
             f' -f "Unused Healing|!hp {str(damage.total)}|inline" '
             )
-cc_current = cc_str(cc_name)
 return_string += (
-    f' -f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-    f' -footer "{ctx.prefix}{ctx.alias} [crit(n)] [charges(1)] [?res/vuln]" '
+    f' -footer "{ctx.prefix}{ctx.alias} [?strikes (crits count as 2)] [?allCrit] [?res/vuln] [?noHeal (stores adv)]" '
     )
 return return_string
 </drac2>
