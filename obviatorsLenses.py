5e397ccc4f114b7eed1ec8bd32ab4492fdfd43f5
derekshum <derekqlshum@gmail.com>
2021-11-02 20:46:47 -0400

    Update obviatorsLenses.py
    Update to single attack.

--- obviatorsLenses.py
+++ obviatorsLenses.py
@@ -5,10 +5,10 @@ cc_value = character().get_cc(cc_name)
 if cc_value >= 1:
     character().mod_cc(cc_name, -1)
     return_string = (
-        f' adv ea -d "1d10" -f "{cc_name}|You can focus the power of the lenses to gain accuracy in combat, gaining advantage on a weapon attack roll (no action required). If that attack hits, roll one additional weapon damage die.\n\n**{cc_name}:** {cc_str(cc_name)}"'
+        f' adv1 ea1 -d1 "1d10" -f "{cc_name}|You can focus the power of the lenses to gain accuracy in combat, gaining advantage on a weapon attack roll (no action required). If that attack hits, roll one additional weapon damage die.\n**{cc_name}:** {cc_str(cc_name)}"'
         )
 else:
     return_string = (
-        f' -f "{cc_name} has already been used|This property of the lenses can\'t be used again until the next dawn.\n\n**{cc_name}:** {cc_str(cc_name)}"')
+        f' -f "{cc_name} has already been used|This property of the lenses can\'t be used again until the next dawn.\n**{cc_name}:** {cc_str(cc_name)}"')
 return return_string
 </drac2>
