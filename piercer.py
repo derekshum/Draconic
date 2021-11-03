c0e9e96305fea1de31333bfffeae02f25b7e7b0e
derekshum <derekqlshum@gmail.com>
2021-11-02 21:07:48 -0400

    Update piercer.py
    Making name agnostic.

--- piercer.py
+++ piercer.py
@@ -8,7 +8,7 @@ ability_name = "Piercer"
 damage = vroll("1d12-" + old_roll)
 return_string = (
     f' -title "{name} uses {ability_name}!"'
-    f' -desc "{name}\'s  deadly accuracy inflicts an additional {str(damage)} piercing damage. Once per turn, when Gud hits a creature with an attack that deals piercing damage, Gud can reroll one of the attack’s damage dice, and must use the new roll."'
+    f' -desc "{name}\'s  deadly accuracy inflicts an additional {str(damage)} piercing damage. Once per turn, when they hit a creature with an attack that deals piercing damage, they can reroll one of the attack’s damage dice, and must use the new roll."'
     f' -footer "{ctx.prefix}{ctx.alias} [original roll (1)]"'
     )
 return return_string
