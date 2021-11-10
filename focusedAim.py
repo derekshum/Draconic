d4adb1818ce1bb6327bd564831729a5eef51185c
derekshum <derekqlshum@gmail.com>
2021-11-10 16:03:14 -0500

    Update focusedAim.py
    Only display new total if old total entered.

--- focusedAim.py
+++ focusedAim.py
@@ -1,4 +1,4 @@
-!alias focused tembed
+!alias f tembed
 <drac2>
 additional_disp = False
 if len(&ARGS&) > 0:
@@ -32,16 +32,15 @@ else:
     return_string = (
         f' -title "{name} uses {ability_name}!" '
         f' -desc "When you miss with an attack roll, you can spend 1 to 3 ki points to increase your attack roll by 2 for each of these ki points you spend, potentially turning the miss into a hit." '
-        f' -f "Attack Roll Bonus | +{attack_roll_bonus}|inline" '
         )
     if additional_disp:
-        return_string += (
-            f'-f "New Attack Roll | {disp_num} + {attack_roll_bonus} = {disp_num + attack_roll_bonus}|inline" '
-            )
+        return_string += f' -f "New Attack Roll | {disp_num} + {attack_roll_bonus} = {disp_num + attack_roll_bonus}|inline" '
+    else:
+        return_string += f' -f "Attack Roll Bonus | +{attack_roll_bonus}|inline" '
 cc_current = cc_str(cc_name)
 return_string += (
-    f'-f "{cc_name} (-{cc_use})| {cc_current}|inline" '
-    f'-footer "{ctx.prefix}{ctx.alias} [# ki (optional, 1-3, default 1)] [initial attack roll]"'
+    f' -f "{cc_name} (-{cc_use})| {cc_current}|inline" '
+    f' -footer "{ctx.prefix}{ctx.alias} [# ki (optional, 1-3, default 1)] [initial attack roll]" '
     )
 return return_string
 </drac2>
