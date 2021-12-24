33bacecd37a1dab24a8879f563cac3e803500153
derekshum <derekqlshum@gmail.com>
2021-12-23 20:20:59 -0500

    Create foe slayer.py

---
+++ foe slayer.py
@@ -0,0 +1,7 @@
+!alias foe tembed
+<drac2>
+return_string = (
+    f' -title "" '
+    f' -description "Once on each of your turns, you can add your Wisdom modifier **(+{character().WisdomMod})** to the attack roll or the damage roll of an attack you make against one of your favored enemies. You can choose to use this feature before or after the roll, but before any effects of the roll are applied." '
+return return_string
+</drac2>
