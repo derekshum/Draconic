19b21aee14e3610c1f83e27e54d551dd005bbbfb
derekshum <derekqlshum@gmail.com>
2022-01-22 15:29:08 -0500

    Update dragonBreath.py
    fixing text

--- dragonBreath.py
+++ dragonBreath.py
@@ -1,4 +1,4 @@
-#!cc create "Spear of Dragonkind" -min 0 -max 1 -type bubble -reset long -desc "While holding the flail, you can use an action and speak a command word to ~~cause the heads to~~ breathe multicolored flames in a 90-foot cone. Each creature in that area must make a DC 18 Dexterity saving throw. On a failed save, it takes 14d6 damage of one of the following damage types (your choice): acid, cold, fire, lightning, or poison. On a successful save, it takes half as much damage. Once this action is used, it can't be used again until the next dawn."
+#!cc create "Spear of Dragonkind" -min 0 -max 1 -type bubble -reset long -desc "While holding the *Spear of Dragonkind*, you can use an action and speak a command word to ~~cause the heads to~~ breathe multicolored flames in a 90-foot cone. Each creature in that area must make a DC 18 Dexterity saving throw. On a failed save, it takes 14d6 damage of one of the following damage types (your choice): acid, cold, fire, lightning, or poison. On a successful save, it takes half as much damage. Once this action is used, it can't be used again until the next dawn."
 
 !alias db tembed
 <drac2>
@@ -14,7 +14,7 @@ if cc_value >= cc_request:
     damage = vroll(roll_string)
     return_string = (
         f' -title "{name} {ability_name}!" '
-        f' -desc "While holding the *Spear of Dragonkind*, you can use an action and speak a command word to ~~cause the heads to~~ breathe multicolored flames in a 90-foot cone. Each creature in that area must make a **DC 18 Dexterity saving throw**. On a failed save, it takes {str(damage)} damage of one of the following damage types (your choice): acid, cold, fire, lightning, or poison. On a successful save, it takes half as much damage. Once this action is used, it can\'t be used again until the next dawn." '
+        f' -desc "While holding the *Spear of Dragonkind*, you can use an action and speak a command word to ~~cause the heads to~~ breathe multicolored flames in a 90-foot cone. Each creature in that area must make a **DC 18 Dexterity saving throw**. On a failed save, it takes {str(damage)} (half `{int(damage.total / 2)}`) damage of one of the following damage types (your choice): acid, cold, fire, lightning, or poison. On a successful save, it takes half as much damage." '
         
         )
 else:
