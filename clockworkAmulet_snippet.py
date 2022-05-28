dd85cf725c327c8c67381d8770e510e1ffd7525c
derekshum <derekshum@users.noreply.github.com>
2022-05-28 17:53:04 -0400

    utilizing -attackroll for clockwork amulet

--- clockworkAmulet_snippet.py
+++ clockworkAmulet_snippet.py
@@ -9,13 +9,13 @@ if cc_value >= num:
     cc_use = num
     character().mod_cc(cc_name, -cc_use)
     return_string = (
-        f' hit{num} '
+        f' -attackroll{num} 10 '
         f' -f "{cc_name}|When you make an attack roll while wearing the amulet, you can forgo rolling the d20 to get a 10 on the die." '
         )
 else:
     cc_use = 0
     return_string = f' -f "Your {cc_name} has already been used today|This attack was made without this enhancement."'
-cc_current = cc_str(cc_name)
+cc_current = character().cc_str(cc_name)
 return_string += f' -f "{cc_name} (-{cc_use})| {cc_current}|inline"'
 return return_string
 </drac2>
