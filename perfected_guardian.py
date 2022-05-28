dcc8947673e4a2e9d4c3b85bdce4ebec036cd2e5
derekshum <derekshum@users.noreply.github.com>
2022-05-28 16:27:05 -0400

    modify perfect guardian bolding and depreciation

--- perfected_guardian.py
+++ perfected_guardian.py
@@ -1,5 +1,3 @@
-!cc create "Perfected Guardian" -min 0 -max proficiencyBonus -type bubble -reset long -desc "When a Huge or smaller creature you can see ends its turn within 30 feet of you, you can use your reaction to magically force the creature to make a Strength saving throw against your spell save DC, pulling the creature up to 30 feet toward you to an unoccupied space. If you pull the target to a space within 5 feet of you, you can make a melee weapon attack against it as part of this reaction."
-
 !alias pg tembed
 <drac2>
 ability_name = "Perfected Guardian Armor"
@@ -15,7 +13,7 @@ if cc_value >= 1:
     character().mod_cc(cc_name, -cc_use)
     return_string = (
         f'-title "{name} uses their {ability_name}!" '
-        f'-desc "When a Huge or smaller creature you can see ends its turn within 30 feet of you, you can use your reaction to magically force the creature to make a **DC {int(get_raw().spellbook.dc) + bonus}** Strength saving throw, pulling the creature up to 30 feet toward you to an unoccupied space. If you pull the target to a space within 5 feet of you, you can make a melee weapon attack against it as part of this reaction. " '
+        f'-desc "When a Huge or smaller creature you can see ends its turn within 30 feet of you, you can use your reaction to magically force the creature to make a **DC {int(character().spellbook.dc) + bonus} Strength saving throw**, pulling the creature up to 30 feet toward you to an unoccupied space. If you pull the target to a space within 5 feet of you, you can make a melee weapon attack against it as part of this reaction. " '
         )
 else:
     cc_use = 0
@@ -23,10 +21,12 @@ else:
         f' -title "{name} fails to use their {ability_name}!" '
         f' -desc "Take a long rest." '
         )
-cc_current = cc_str(cc_name)
+cc_current = character().cc_str(cc_name)
 return_string += (
     f'-f "{cc_name} (-{cc_use})| {cc_current}|inline" '
     f'-footer "{ctx.prefix}{ctx.alias} [DC bonus]"'
     )
 return return_string
 </drac2>
+
+!cc create "Perfected Guardian" -min 0 -max proficiencyBonus -type bubble -reset long -desc "When a Huge or smaller creature you can see ends its turn within 30 feet of you, you can use your reaction to magically force the creature to make a Strength saving throw against your spell save DC, pulling the creature up to 30 feet toward you to an unoccupied space. If you pull the target to a space within 5 feet of you, you can make a melee weapon attack against it as part of this reaction."
