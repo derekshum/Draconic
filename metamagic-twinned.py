2cfa1fa092131c8fe082dd0f245d2e1f0383897d
derekshum <derekshum@users.noreply.github.com>
2022-05-28 18:38:05 -0400

    imrpovements to twinned

--- metamagic-twinned.py
+++ metamagic-twinned.py
@@ -4,49 +4,49 @@ lvl = 1
 if len(&ARGS&) > 0:
     args = &ARGS&
     lvl = int(args[0].lower())
-ability_name = "Twinned Spell"
-cc_name_1 = "Metamagic Points"
-cc_name_2 = "Sorcery Points"
-cc_value_1 = 0
-if cc_exists(cc_name_1):
-    cc_value_1 = get_cc(cc_name_1)
-cc_value_2 = 0
-if cc_exists(cc_name_2):
-    cc_value_2 = get_cc(cc_name_2)
-cc_use_1 = 0
-cc_use_2 = 0
-if cc_value_1 + cc_value_2 >= lvl:
-    if cc_value_1 >= lvl:
-        cc_use_1 = lvl
-        mod_cc(cc_name_1, -cc_use_1)
-    elif cc_value_1 < lvl and cc_value_1 > 0:
-        cc_use_1 = cc_value_1
-        cc_use_2 = lvl - cc_value_1
-        mod_cc(cc_name_1, -cc_use_1)
-        mod_cc(cc_name_2, -cc_use_2)
+an = "Twinned Spell"
+cn1 = "Metamagic Points"
+cn2 = "Sorcery Points"
+cv1 = 0
+if character().cc_exists(cn1):
+    cv1 = character().get_cc(cn1)
+cv2 = 0
+if character().cc_exists(cn2):
+    cv2 = character().get_cc(cn2)
+cu1 = 0
+cu2 = 0
+if cv1 + cv2 >= lvl:
+    if cv1 >= lvl:
+        cu1 = lvl
+        character().mod_cc(cn1, -cu1)
+    elif cv1 < lvl and cv1 > 0:
+        cu1 = cv1
+        cu2 = lvl - cv1
+        character().mod_cc(cn1, -cu1)
+        character().mod_cc(cn2, -cu2)
     else:
-        cc_use_2 = lvl
-        mod_cc(cc_name_2, -cc_use_2)
+        cu2 = lvl
+        character().mod_cc(cn2, -cu2)
     
-    return_string = (
-        f'-title "{name} uses {ability_name} on a Level {lvl} Spell!" '
+    rs = (
+        f'-title "{name} uses {an} on a Level {lvl} Spell!" '
         f'-desc "When you cast a spell that targets only one creature and doesn\'t have a range of self, you can spend a number of sorcery points equal to the spell\'s level to target a second creature in range with the same spell (1 sorcery point if the spell is a cantrip).\n\nTo be eligible, a spell must be incapable of targeting more than one creature at the spell\'s current level. For example, magic missile and scorching ray aren\'t eligible, but ray of frost and chromatic orb are." '
         )
 else:
-    return_string = (
-        f' -title "{name} fails to use {ability_name} on a Level {lvl} Spell!" '
-        f' -desc "Fewer than {lvl} {cc_name_1} or {cc_name_2} left, take a long rest." '
+    rs = (
+        f' -title "{name} fails to use {an} on a Level {lvl} Spell!" '
+        f' -desc "Fewer than {lvl} {cn1} or {cn2} left, take a long rest." '
         )
-if cc_exists(cc_name_1):
+if character().cc_exists(cn1):
     cc_use_string_1 = ""
-    if cc_use_1 > 0:
-        cc_use_string_1 = " (-" + cc_use_1 + ")"
-    return_string += (f'-f "{cc_name_1} {cc_use_string_1}| {cc_str(cc_name_1)}|inline" ')
-if cc_exists(cc_name_2):
+    if cu1 > 0:
+        cc_use_string_1 = " (-" + cu1 + ")"
+    rs += (f'-f "{cn1} {cc_use_string_1}| {cc_str(cn1)}|inline" ')
+if character().cc_exists(cn2):
     cc_use_string_2 = ""
-    if cc_use_2 > 0:
-        cc_use_string_2 = " (-" + cc_use_2 + ")"
-    return_string += (f'-f "{cc_name_2} {cc_use_string_2}| {cc_str(cc_name_2)}|inline" ')
-return_string += (f'-footer "{ctx.prefix}{ctx.alias}"')
-return return_string
+    if cu2 > 0:
+        cc_use_string_2 = " (-" + cu2 + ")"
+    rs += (f'-f "{cn2} {cc_use_string_2}| {cc_str(cn2)}|inline" ')
+rs += (f'-footer "{ctx.prefix}{ctx.alias}"')
+return rs
 </drac2>
--- metamagic-twinned1_snippet.py
+++ metamagic-twinned1_snippet.py
@@ -1,39 +1,40 @@
 !snippet twinned1 tembed
 <drac2>
+lvl = 1
 ability_name = "Twinned Spell"
 cc_name_1 = "Metamagic Points"
 cc_name_2 = "Sorcery Points"
 cc_value_1 = 0
-if cc_exists(cc_name_1):
-    cc_value_1 = get_cc(cc_name_1)
+if character().cc_exists(cc_name_1):
+    cc_value_1 = character().get_cc(cc_name_1)
 cc_value_2 = 0
-if cc_exists(cc_name_2):
-    cc_value_2 = get_cc(cc_name_2)
+if character().cc_exists(cc_name_2):
+    cc_value_2 = character().get_cc(cc_name_2)
 cc_use_1 = 0
 cc_use_2 = 0
-if cc_value_1 + cc_value_2 >= 1:
+if cc_value_1 + cc_value_2 >= lvl:
     if cc_value_1 > 0:
-        cc_use_1 = 1
-        mod_cc(cc_name_1, -cc_use_1)
+        cc_use_1 = lvl
+        character().mod_cc(cc_name_1, -cc_use_1)
     else:    #cc_value_2 > 1:
-        cc_use_2 = 1
-        mod_cc(cc_name_2, -cc_use_2)
+        cc_use_2 = lvl
+        character().mod_cc(cc_name_2, -cc_use_2)
     return_string = (
-        f'-f "{ability_name} (1)|When you cast a spell that targets only one creature and doesn\'t have a range of self, you can spend a number of sorcery points equal to the spell\'s level to target a second creature in range with the same spell (1 sorcery point if the spell is a cantrip).\n\nTo be eligible, a spell must be incapable of targeting more than one creature at the spell\'s current level. For example, magic missile and scorching ray aren\'t eligible, but ray of frost and chromatic orb are." '
+        f' -l {lvl} -f "{ability_name} (1)|When you cast a spell that targets only one creature and doesn\'t have a range of self, you can spend a number of sorcery points equal to the spell\'s level to target a second creature in range with the same spell (1 sorcery point if the spell is a cantrip).\n\nTo be eligible, a spell must be incapable of targeting more than one creature at the spell\'s current level. For example, magic missile and scorching ray aren\'t eligible, but ray of frost and chromatic orb are." '
         )
 else:
     return_string = (
         f' -f "Failure to use {ability_name} (1)|No {cc_name_1} or {cc_name_2} left, take a long rest." '
         )
-if cc_exists(cc_name_1):
+if character().cc_exists(cc_name_1):
     cc_use_string_1 = ""
     if cc_use_1 > 0:
         cc_use_string_1 = " (-" + cc_use_1 + ")"
-    return_string += (f'-f "{cc_name_1} {cc_use_string_1}| {cc_str(cc_name_1)}|inline" ')
-if cc_exists(cc_name_2):
+    return_string += (f'-f "{cc_name_1} {cc_use_string_1}| {character().cc_str(cc_name_1)}|inline" ')
+if character().cc_exists(cc_name_2):
     cc_use_string_2 = ""
     if cc_use_2 > 0:
         cc_use_string_2 = " (-" + cc_use_2 + ")"
-    return_string += (f'-f "{cc_name_2} {cc_use_string_2}| {cc_str(cc_name_2)}|inline" ')
+    return_string += (f'-f "{cc_name_2} {cc_use_string_2}| {character().cc_str(cc_name_2)}|inline" ')
 return return_string
 </drac2>
