c5476b047b1108dc8e28368326ae2c052d785f92
derekshum <derekshum@users.noreply.github.com>
2022-11-27 10:40:21 -0500

    dragon starry form

---
+++ starryDragonForm.py
@@ -0,0 +1,27 @@
+!alias dsf tembed
+<drac2>
+cc_request = 1
+ability_name = "Dragon Starry Form"
+cc_name = "Wild Shape"
+cc_value = character().get_cc(cc_name)
+return_string = ""
+if cc_value >= cc_request:
+    cc_use = cc_request
+    character().mod_cc(cc_name, -cc_use)
+    return_string = (
+        f' -title "{name} uses {ability_name}!" '
+        f' -desc "As a bonus action, you can expend a use of your Wild Shape feature to take on a starry form, rather than transforming into a beast.\n\nWhile in your starry form, you retain your game statistics, but your body becomes luminous; your joints glimmer like stars, and glowing lines connect them as on a star chart. This form sheds bright light in a 10-foot radius and dim light for an additional 10 feet. The form lasts for 10 minutes. It ends early if you dismiss it (no action required), are incapacitated, die, or use this feature again.\n\n**Dragon.** A constellation of a wise dragon appears on you. When you make an Intelligence or a Wisdom check or a Constitution saving throw to maintain concentration on a spell, you can treat a roll of 9 or lower on the d20 as a 10." '
+        )
+else:
+    cc_use = 0
+    return_string = (
+        f' -title "{name} fails to use {ability_name}!" '
+        f' -desc "No {cc_name} remaining." '
+        )
+cc_current = character().cc_str(cc_name)
+return_string += (
+    f' -f "{cc_name} (-{cc_use})| {cc_current}|inline" '
+    f' -footer "{ctx.prefix}{ctx.alias}" '
+    )
+return return_string
+</drac2>
