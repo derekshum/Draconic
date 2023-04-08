955f02c76ec0e6701458874f85c04c914c8ae04c
derekshum <derekshum@users.noreply.github.com>
2023-04-08 09:58:32 -0400

    psionic strike updates

--- psionicStrike.py
+++ psionicStrike.py
@@ -1,4 +1,5 @@
-!alias ps tembed
+!alias ps 
+tembed
 <drac2>
 cc_request = 1
 is_resistant = False
@@ -18,8 +19,15 @@ if cc_value >= cc_request:
     cc_use = cc_request
     character().mod_cc(cc_name, -cc_use)
     if is_resistant and is_vulnerable:     
-        return_string += f' -f "Calculation Uncertainty|Resisted and Vulnerable damage may have a lower total than displayed due to rounding down odds." '    
-    roll_string = f'1d6+{intelligenceMod}' #not set up for level 5+ psi warrior
+        return_string += f' -f "Calculation Uncertainty|Resisted and Vulnerable damage may have a lower total than displayed due to rounding down odds." '  
+    roll_string='1d6'
+    if level >= 17:
+        roll_string='1d12'
+    elif level >= 11:
+        roll_string='1d10'
+    elif level >= 5:
+        roll_string='1d8'
+    roll_string += f'+{intelligenceMod}'
     if is_resistant:
         roll_string = "(" + roll_string + ")/2"
     if is_vulnerable:
@@ -27,7 +35,7 @@ if cc_value >= cc_request:
     damage = vroll(roll_string)
     return_string = (
         f' -title "{name} uses {ability_name}!" '
-        f' -desc "Once on each of your turns, immediately after you hit a target within 30 feet of you with an attack and deal damage to it with a weapon, you can expend one Psionic Energy die, rolling it and dealing force damage to the target equal to the number rolled plus your Intelligence modifier." '
+        f' -desc "Once on each of your turns, immediately after you hit a target within 30 feet of you with an attack and deal damage to it with a weapon, you can expend one Psionic Energy die, rolling it and dealing force damage to the target equal to the number rolled plus your Intelligence modifier.\n\nWhen you deal damage to a target with your Psionic Strike, you can force the target to make a **DC {8 + proficiencyBonus + intelligenceMod} Strength saving throw**. If the save fails, you can knock the target prone or move it up to 10 feet in any direction horizontally." '
         f' -f "Damage|{str(damage)}[force]|inline" '
         )
 else:
