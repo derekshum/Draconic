49a03bac0c2d71a2246925ab650a6e7797b91ef1
derekshum <derekshum@users.noreply.github.com>
2023-06-15 23:34:55 -0400

    Unarmed Fighting fixes

--- unarmedFighting.py
+++ unarmedFighting.py
@@ -1,10 +1,9 @@
-!alias fr2 tembed
+!alias uf tembed
 <drac2>
 damage = vroll("1d4")
 return_string = (
-    f' -title "{name}\'s Fire Rune burns the target!" '
-    f' -desc "While restrained by the shackles, the target takes 2d6 fire damage at the start of each of its turns. The target can repeat the **DC {8+proficiencyBonus+constitutionMod} Strength saving throw** at the end of each of its turns, banishing the shackles on a success." '
-    f' -f "Damage|{str(damage)}|inline" '
+    f' -title "{name}\'s Unarmed Fighting gives a grappled target a good shake!" '
+    f' -desc "At the start of each of your turns, you can deal {damage} bludgeoning damage to one creature grappled by you." '
 )
 return_string += f' -footer "{ctx.prefix}{ctx.alias}" '
 return return_string
